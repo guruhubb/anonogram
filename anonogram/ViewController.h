6a50dfa96c22eea821902da1425fa1d6c0c6d872
sb2012 <saswata_basu@yahoo.com>
2014-04-22 17:36:47 -0700

    Initial Commit

---
+++ anonogram/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  anonogram
+//
+//  Created by Saswata Basu on 4/22/14.
+//  Copyright (c) 2014 Saswata Basu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ anonogramTests/anonogramTests.m
@@ -0,0 +1,34 @@
+//
+//  anonogramTests.m
+//  anonogramTests
+//
+//  Created by Saswata Basu on 4/22/14.
+//  Copyright (c) 2014 Saswata Basu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface anonogramTests : XCTestCase
+
+@end
+
+@implementation anonogramTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
